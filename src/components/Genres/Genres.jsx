9d04674792c4e1eeee95a38869c3376e251b75df
Joaquin Bohn <joaquinbohnv@gmail.com>
2023-05-23 11:35:18 -0300

    Deslizamiento de tarjetas con animaciones

--- src/components/Genres/Genres.jsx
+++ src/components/Genres/Genres.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import "./Genres.css";
 
 const Genres = () => {
+  const [slidePosition, setSlidePosition] = useState(0);
+  const [showBackwardArrow, setShowBackwardArrow] = useState(false);
+  const [showForwardArrow, setShowForwardArrow] = useState(true);
+
+  const moveBackward = () => {
+    if (slidePosition > 0) {
+      setSlidePosition(slidePosition - 1);
+    }
+  };
+
+  const moveForward = () => {
+    if (slidePosition < 2) {
+      setSlidePosition(slidePosition + 1);
+    }
+  };
+
+  useEffect(() => {
+    slidePosition === 0
+      ? setShowBackwardArrow(false)
+      : setShowBackwardArrow(true);
+    slidePosition < 1 ? setShowForwardArrow(true) : setShowForwardArrow(false);
+  }, [slidePosition]);
+
   return (
     <div className="genres-container">
       <h2 className="genres-title">Explore by genre</h2>
-      <div className="genres">
+      <div
+        className="genres"
+        id={showBackwardArrow ? "move-forward" : "move-backward"}
+      >
         <div className="genre-card" id="romance">
           <div className="genre-name">
             <h3>Romance</h3>
@@ -36,6 +64,30 @@ const Genres = () => {
             <h3>Drama</h3>
           </div>
         </div>
+      </div>
+      <div className="genre-arrow">
+        <button
+          className="arrow-button"
+          id={showBackwardArrow ? "arrow-show" : "arrow-hidden"}
+          onClick={moveBackward}
+        >
+          <ArrowBackIosIcon className="arrow-icon" sx={{ fontSize: "40px" }} />
+        </button>
+        <div
+          className={
+            showBackwardArrow && showForwardArrow ? "aux-hidden" : "aux-show"
+          }
+        ></div>
+        <button
+          className="arrow-button"
+          id={showForwardArrow ? "arrow-show" : "arrow-hidden"}
+          onClick={moveForward}
+        >
+          <ArrowForwardIosIcon
+            className="arrow-icon"
+            sx={{ fontSize: "40px" }}
+          />
+        </button>
       </div>
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,10 +2,32 @@ import { useEffect, useState } from "react";
 import { db } from "../../firebaseConfig";
 import { getDocs, collection, query, limit } from "firebase/firestore";
 import "./ItemListContainer.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ItemList from "../ItemList/ItemList";
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
+  const [listPosition, setListPosition] = useState(0);
+  const [showBackArrow, setShowBackArrow] = useState(false);
+  const [showForArrow, setShowForArrow] = useState(true);
+
+  const moveBack = () => {
+    if (listPosition > 0) {
+      setListPosition(listPosition - 1);
+    }
+  };
+
+  const moveFor = () => {
+    if (listPosition < 2) {
+      setListPosition(listPosition + 1);
+    }
+  };
+
+  useEffect(() => {
+    listPosition === 0 ? setShowBackArrow(false) : setShowBackArrow(true);
+    listPosition < 1 ? setShowForArrow(true) : setShowForArrow(false);
+  }, [listPosition]);
 
   useEffect(() => {
     const itemCollection = collection(db, "contenido");
@@ -28,8 +50,38 @@ const ItemListContainer = () => {
 
   return (
     <div>
-      <div className="itemListContainer">
+      <div
+        className="itemListContainer"
+        id={showBackArrow ? "move-forw" : "move-backw"}
+      >
         <ItemList items={items} />
+      </div>
+      <div className="itemList-arrow">
+        <button
+          className="arrow-list-button"
+          id={showBackArrow ? "arrow-list-show" : "arrow-list-hidden"}
+          onClick={moveBack}
+        >
+          <ArrowBackIosIcon
+            className="arrow-list-icon"
+            sx={{ fontSize: "40px" }}
+          />
+        </button>
+        <div
+          className={
+            showBackArrow && showForArrow ? "aux-list-hidden" : "aux-list-show"
+          }
+        ></div>
+        <button
+          className="arrow-list-button"
+          id={showForArrow ? "arrow-list-show" : "arrow-list-hidden"}
+          onClick={moveFor}
+        >
+          <ArrowForwardIosIcon
+            className="arrow-list-icon"
+            sx={{ fontSize: "40px" }}
+          />
+        </button>
       </div>
     </div>
   );
